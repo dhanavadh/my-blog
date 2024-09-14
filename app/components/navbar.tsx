5587d70dda755ff2077f2abc84b34ec10a93a62c
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2024-09-14 12:11:00 +0700

    rm git button
    remove git button

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -60,14 +60,8 @@ function Navbar() {
                         <a className={color ? navtext[1] : navtext[0]} href="/experience">
                                 Experience
                             </a>
-                        </div>
-
-                        <div className="items-center hidden space-x-5 sm:flex">
-                            <a className={color ? btnDec[1] : btnDec[0]} href="https://github.com/dhanavadh" target="_blank">
+                        <a className={color ? navtext[1] : navtext[0]} href="https://github.com/dhanavadh" target="_blank">
                                 GitHub
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ml-1">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
-                                </svg>
                             </a>
                         </div>
                         <div>
